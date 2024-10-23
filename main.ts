bad69cd6bc54801b058895dd32490b05f40e531a
LegoWorksStudent <122985953+legoworksstudents@users.noreply.github.com>
2024-10-23 16:57:13 +0200

    Actualizar main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,13 +1,15 @@
 basic.forever(function () {
-    if (PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.TrackingStateType.Tracking_State_1)) {
-        neZha.setMotorSpeed(neZha.MotorList.M1, -60)
-        neZha.setMotorSpeed(neZha.MotorList.M2, -60)
+    // hola iñaki!!!!!!
+    if (!(PlanetX_Basic.trackingSensor(PlanetX_Basic.DigitalRJPin.J2, PlanetX_Basic.TrackingStateType.Tracking_State_3))) {
+        neZha.setMotorSpeed(neZha.MotorList.M1, -20)
+        neZha.setMotorSpeed(neZha.MotorList.M4, 20)
     } else {
-        if (PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) > 50) {
-            neZha.setMotorSpeed(neZha.MotorList.M1, 60)
-            neZha.setMotorSpeed(neZha.MotorList.M2, 60)
+        if (PlanetX_Basic.ultrasoundSensor(PlanetX_Basic.DigitalRJPin.J1, PlanetX_Basic.Distance_Unit_List.Distance_Unit_cm) > 40) {
+            neZha.setMotorSpeed(neZha.MotorList.M1, 30)
+            neZha.setMotorSpeed(neZha.MotorList.M4, 40)
         } else {
-            neZha.setServoSpeed(neZha.ServoList.S1, 60)
+            neZha.setMotorSpeed(neZha.MotorList.M1, 40)
+            neZha.setMotorSpeed(neZha.MotorList.M4, -40)
         }
     }
 })
